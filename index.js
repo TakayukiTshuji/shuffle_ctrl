f8552f1e243e70416d188a3b4da67ce1ef8ccf27
TakayukiTshuji <gp22a109@oecu.jp>
2025-12-15 22:49:20 +0900

    feat: カードシャッフルをWebベースからWebアプリベースに変更

---
+++ index.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow, screen } = require('electron/main');
+
+const createWindow = () => {
+  
+  const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+
+  const win = new BrowserWindow({
+    width,
+    height,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  win.loadFile('index.html');
+};
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
