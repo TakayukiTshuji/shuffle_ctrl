096123edbe417bb3f577f23d05c1e927359611e2
TakayukiTshuji <gp22a109@oecu.jp>
2025-12-17 23:43:30 +0900

    Optimized : ウィンドウサイズの調整

--- index.js
+++ index.js
@@ -1,6 +1,53 @@
 const { app, BrowserWindow, screen, Menu } = require('electron/main');
 const path = require('path');
-//const { spawn } = require('child_process');
+
+// インストール/アンインストール時にショートカットを作成・削除する関数
+const handleSquirrelEvent = () => {
+  if (process.argv.length === 1) {
+    return false;
+  }
+
+  const ChildProcess = require('child_process');
+  const path = require('path');
+
+  const appFolder = path.resolve(process.execPath, '..');
+  const rootAtomFolder = path.resolve(appFolder, '..');
+  const updateDotExe = path.resolve(path.join(rootAtomFolder, 'Update.exe'));
+  const exeName = path.basename(process.execPath);
+
+  const spawnUpdate = function(args) {
+    let spawnedProcess, error;
+    try {
+      spawnedProcess = ChildProcess.spawn(updateDotExe, args, { detached: true });
+    } catch (error) {}
+    return spawnedProcess;
+  };
+
+  const squirrelEvent = process.argv[1];
+  switch (squirrelEvent) {
+    case '--squirrel-install':
+    case '--squirrel-updated':
+      // ここでデスクトップとスタートメニューにショートカットを作成します
+      spawnUpdate(['--createShortcut', exeName]);
+      setTimeout(app.quit, 1000);
+      return true;
+
+    case '--squirrel-uninstall':
+      // アンインストール時にショートカットを削除します
+      spawnUpdate(['--removeShortcut', exeName]);
+      setTimeout(app.quit, 1000);
+      return true;
+
+    case '--squirrel-obsolete':
+      app.quit();
+      return true;
+  }
+};
+
+// この関数が true を返したら（インストール処理中なら）、アプリ本体は起動せずに終了する
+if (handleSquirrelEvent()) {
+  return;
+}
 
 try {
   const updateElectronApp = require('update-electron-app');
@@ -19,7 +66,7 @@ const createWindow = () => {
     icon: path.join(__dirname, './img/playingcard.ico'),
     webPreferences: {
       nodeIntegration: true,
-      zoomFactor: 0.45,
+      zoomFactor: 0.46,
     },
   });
 
